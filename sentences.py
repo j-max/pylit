62d5ee9ca239b05f23c7cf8c9a69cf35998f13fd
j-max <jmax.barry@gmail.com>
2024-02-03 10:09:47 -0600

    Sentence function returns most similar sentence, but it isn't very similar, by visual inspection

--- sentences.py
+++ sentences.py
@@ -1,5 +1,8 @@
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk.tokenize import sent_tokenize
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 from sentiment import calculate_sentence_sentiment
 
@@ -16,6 +19,47 @@ def find_sentences(document_string):
     sentences = [Sentence(sentence_string) for sentence_string in tokenized_sentences]
     
     return sentences
+
+
+def count_vectorize_sentences(list_of_sentences):
+
+    sentence_strings = [sentence.sentence_string for sentence in list_of_sentences]
+    
+    cv = CountVectorizer(stop_words="english")
+    cv_sentences = np.array(cv.fit_transform(sentence_strings).todense())
+
+    for sentence, cv_sentence in zip(list_of_sentences, cv_sentences):
+        sentence.cv_sentence = cv_sentence
+
+    return cv_sentences
+
+
+def find_most_similar_sentence(
+    target_sentence,
+    list_of_sentences
+):
+
+    # make a list of all count vectorized forms of a sentence
+    cv_sentences = [sentence.cv_sentence for sentence in list_of_sentences]
+    
+    # define an array of cosine similarities of all sentences compared to a target sentence
+    sentence_similarities = cosine_similarity(
+        cv_sentences, [target_sentence.cv_sentence]
+    )
+
+    highest_similarity_score = 0
+    highest_similarity_index = 0
+    for i, similarity_score in enumerate(sentence_similarities):
+        # A similarity score of 1 means it is most likely the same sentence
+        # there could be sentences with the same words in a different order.
+        if similarity_score[0] <= .999999 and similarity_score > highest_similarity_score:
+            highest_similarity_score = similarity_score
+            highest_similarity_index = i
+    
+    print(highest_similarity_score)
+    print(highest_similarity_index)
+
+    return list_of_sentences[highest_similarity_index].sentence_string
 
 
 class Sentence:
@@ -46,6 +90,7 @@ class Sentence:
         self.word_count = len(self.word_tokens)
         self.sentiment_score = calculate_sentence_sentiment(self.sentence_string)
         self.date = ''
+        self.cv_sentence = []
         # TODO Add timestamps to journal sentences
         # TODO Add POS to the tokens
         
